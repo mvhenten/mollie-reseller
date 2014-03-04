80e68af7dec923e3e1821b0d2187c831c4f77932
Matthijs van Henten <matthijs@ischen.nl>
2014-03-05 00:22:04 +0100

    adding reference script

---
+++ scripts/account-create.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var API = require('../lib/api');
+
+var api = new API({
+    partner_id: process.env.MOLLIE_PARTNER_ID,
+    profile_key: process.env.MOLLIE_PROFILE_KEY,
+    app_secret: process.env.MOLLIE_APP_SECRET,
+    action: 'account-create'
+});
+
+api.post({
+    username: 'my_customer',
+
+    name: 'John Doe',
+
+    email: 'info@ischen.nl',
+
+    address: 'Damstraat 1',
+
+    zipcode: '1011AB',
+
+    city: 'Amsterdam',
+
+    country: '1'
+
+}, function() {
+    process.exit();
+});
