327e1efebd5262a3d099b08625d7cd19af7af573
Matthijs van Henten <matthijs@ischen.nl>
2014-03-05 01:14:21 +0100

    updated reference script

--- scripts/account-create.js
+++ scripts/account-create.js
@@ -2,32 +2,24 @@
 
 'use strict';
 
-var API = require('../lib/api');
+var Reseller = require('../lib/reseller');
 
-var api = new API({
+var api = new Reseller({
     partner_id: process.env.MOLLIE_PARTNER_ID,
     profile_key: process.env.MOLLIE_PROFILE_KEY,
-    app_secret: process.env.MOLLIE_APP_SECRET,
-    action: 'account-create'
+    app_secret: process.env.MOLLIE_APP_SECRET
 });
 
-api.post({
+api.accountCreate({
     username: 'my_customer',
-
     name: 'John Doe',
-
-    email: 'info@ischen.nl',
-
+    email: 'john+doe@example.com',
     address: 'Damstraat 1',
-
     zipcode: '1011AB',
-
     city: 'Amsterdam',
-
-    country: '1'
-
+    country: '1',
+    testmode: true,
 }, function(err, result) {
     console.log(result);
-
     process.exit();
 });
