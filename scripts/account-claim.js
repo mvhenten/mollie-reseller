3f5f9f44259290b93f24374e37e5f4e711ad48be
Matthijs van Henten <matthijs@ischen.nl>
2014-03-05 12:50:51 +0100

    adding a number of reference scripts

---
+++ scripts/account-claim.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var Reseller = require('../lib/reseller');
+
+var api = new Reseller({
+    partner_id: process.env.MOLLIE_PARTNER_ID,
+    profile_key: process.env.MOLLIE_PROFILE_KEY,
+    app_secret: process.env.MOLLIE_APP_SECRET
+});
+
+api.accountClaim({
+    username: '<real username>',
+    password: '<real password>',
+}, function(err, result) {
+    console.log(result);
+    process.exit();
+});
